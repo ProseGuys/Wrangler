97d4e9d0242a815a1b7020e36891787818947351
ProseGuys <798770222@qq.com>
2024-10-14 17:30:09 +0800

    [feat] add pdf handler

--- main.py
+++ main.py
@@ -3,43 +3,40 @@
 # Description: None
 import logging
 import os
+import pathlib
+import re
 import shutil
 import subprocess
+from typing import List, Optional
 
+import click
 import docker
 import docker.errors
+import pymupdf
+from markdown_it import MarkdownIt
+from tqdm import tqdm
 
 from config import (
     BASE_PATH,
     DOCKER_MODE,
-    EXEC_MODES,
     EXTRACTED_FILE_PATH,
     LOCAL_MODE,
     ORIGIN_FILE_PATH,
 )
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
-def run_command(command: str, is_log: bool = False):
-    process = subprocess.Popen(
-        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True
-    )
-    if is_log:
-        while True:
-            output = process.stdout.readline()
-            if output == "" and process.poll() is not None:
-                break
-            if output:
-                logger.info(output.strip())  # 实时打印输出
-    if process.returncode != 0:
-        raise subprocess.SubprocessError(f"command error: {process.communicate()[-1]}")
+def run_command(command: str):
+    result = subprocess.run(command, text=True, shell=True)
+    if result.returncode != 0:
+        raise subprocess.SubprocessError(f"commnad error, check please:{command}")
 
 
 def get_mode_command(mode: str, pdf_path: str):
-    path_name = pdf_path.split()[-1]
+    path_name = os.path.split(pdf_path)[-1]
     if mode is DOCKER_MODE:
-        run_command(f"mv ")
         command = f"docker run -v {BASE_PATH}:/volumn -v $(pwd)/magic-pdf.json:/root/magic-pdf.json --gpus=all mineru:latest /bin/bash -c 'magic-pdf -p /volumn/origin_files/{path_name} -o /volumn/extrated_files'"
     if mode is LOCAL_MODE:
         command = f"magic-pdf -p {pdf_path} -o {EXTRACTED_FILE_PATH}"
@@ -57,33 +54,144 @@ def check_commad_exist():
         return None, False
 
 
-def wrangler():
-    pass
+def handle_table_for_md(tokens):
+    # 处理表格异常
+    handle_tokens, length, index = [], len(tokens), 0
+    while length > index:
+        token = tokens[index]
+        if "</table>" in token:
+            table_tokens, final_list = [], []
+            for _index in range(index, length):
+                _token = tokens[_index]
+                if "</table>" in _token:
+                    table_tokens.append(re.sub(r"^<td>|</td>$", "", _token.strip()))
+                else:
+                    index = _index
+                    break
+            table_tokens_length = len(table_tokens)
+            if table_tokens_length == 2:
+                for _idx, _tk in enumerate(table_tokens):
+                    if _idx == 0:
+                        _tk = re.sub(r"</table>$", "", _tk)
+                    else:
+                        _tk = re.sub(r"^<table[^>]*>", "", _tk)
+                    final_list.append(_tk)
+            elif table_tokens_length > 2:
+                for _idx, _tk in enumerate(table_tokens):
+                    if _idx == 0:
+                        _tk = re.sub(r"</table>$", "", _tk)
+                    elif _idx == table_tokens_length - 1:
+                        _tk = re.sub(r"^<table[^>]*>", "", _tk)
+                    else:
+                        _tk = re.sub(r"^<table[^>]*>|</table>$", "", _tk)
+                    final_list.append(_tk)
+            else:
+                final_list = table_tokens
+
+            token = "".join(final_list)
+        index += 1
+
+        handle_tokens.append(token)
+    return handle_tokens
 
 
-def extractor(pdf_path: str):
+def handle_header_for_md(tokens, toc: Optional[List] = None):
+    if not toc:
+        return tokens
+    toc_index, toc_length = 0, len(toc) - 1
+    handle_tokens = []
+    for token in tokens:
+        _token = token.strip("#").strip()
+        print(f"====={token}")
+        if toc_index < toc_length:
+            header_level, header_content, _ = toc[toc_index]
+            print(f"------{header_content}")
+            if header_content == _token:
+                print("----------")
+                _token = f"{'#'*header_level} {_token}"
+                toc_index += 1
+        handle_tokens.append(_token)
+    return handle_tokens
+
+
+def handle_md(md_file, toc):
+    with open(md_file, "r") as f:
+        _file = f.read()
+
+    tokens = _file.split("\n\n")
+    tokens = handle_table_for_md(tokens)
+    tokens = handle_header_for_md(tokens, toc)
+    return "\n\n".join(tokens)
+
+
+def single_wrangler(origin_doc, handler_file, doc_name):
+    # choose mineru's file
+    handler_type_folder = os.listdir(handler_file)[0]
+    current_handler_file = os.path.join(
+        handler_file, handler_type_folder, f"{doc_name}.md"
+    )
+    doc = pymupdf.open(origin_doc)
+    toc = doc.get_toc()
+    md_content = handle_md(current_handler_file, toc)
+    md = MarkdownIt()
+    return md.render(md_content)
+
+
+def wrangler(extractor_path: str, output_path: str):
+    documents = [extractor_path]
+    if os.path.isdir(extractor_path):
+        documents = [
+            file_name.split(".")[0]
+            for file_name in os.listdir(extractor_path)
+            if os.path.isfile(os.path.join(extractor_path, file_name))
+        ]
+    # 每个文件单独处理
+    for origin_doc in tqdm(documents, desc="File processing..."):
+        doc_name = pathlib.Path(origin_doc).name.split(".")[0]
+        handler_file = os.path.join(EXTRACTED_FILE_PATH, doc_name)
+        html_content = single_wrangler(origin_doc, handler_file, doc_name)
+        with open(os.path.join(output_path, f"{doc_name}.md"), "w") as f:
+            f.write(html_content)
+
+
+def extractor(extractor_path: str):
     mode, exist = check_commad_exist()
     if not exist:
         raise ModuleNotFoundError(
             "Missing PDF extractor moudle: MinerU, Please install it first"
         )
-    command = get_mode_command(mode, pdf_path)
-    # 执行转换脚本
-    logger.info(command)
-    run_command(command, is_log=True)
+    command = get_mode_command(mode, extractor_path)
+    run_command(command)
 
 
-def main(pdf_path: str, output_path):
+@click.command()
+@click.option(
+    "-p",
+    "--file_path",
+    prompt="files that need to be parsed, file or folder",
+    required=True,
+    type=str,
+)
+@click.option(
+    "-o",
+    "--output_folder",
+    prompt="output_folder_path",
+    required=True,
+    type=str,
+)
+def main(file_path: str, output_folder: str):
+    if not os.path.exists(BASE_PATH):
+        for path in [BASE_PATH, EXTRACTED_FILE_PATH, ORIGIN_FILE_PATH]:
+            os.mkdir(path)
+    if not os.path.exists(output_folder) and os.path.isdir(output_folder):
+        raise NotADirectoryError("directory not exist or the path is not a directory")
     # 进行文件转移
-    run_command(f"cp -r {pdf_path} {ORIGIN_FILE_PATH}")
-    pdf_name = os.path.split(pdf_path)[-1]
-    pdf_path = os.path.join(ORIGIN_FILE_PATH, pdf_name)
-    # 数据提取
-    extractor(pdf_path)
-    # 数据清洗
-    wrangler(pdf_path)
-    # 数据整理
+    run_command(f"cp -r {file_path} {ORIGIN_FILE_PATH}")
+    path_name = os.path.split(file_path)[-1]
+    extractor_path = os.path.join(ORIGIN_FILE_PATH, path_name)
+    extractor(extractor_path)
+    wrangler(extractor_path, output_folder)
 
 
 if __name__ == "__main__":
-    pass
+    main()
